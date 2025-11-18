45e912e381502297153328a6ff0f38b0375a1a05
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:46:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "backend_url" {
-  value = azurerm_linux_web_app.app.default_hostname
-  description = "Medusa backend URL"
+  value = "https://${module.app.default_hostname}"
 }
+
 
 output "acr_name" {
   value = module.acr.name
