b6678e9dbc0f6d84b13b527366923fcc89b06787
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:33:56 +0530

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -20,5 +20,3 @@ variable "db_username" { type = string }
 variable "db_password" { type = string }
 
 variable "branding_store_name" { type = string }
-
-variable "tags" { type = map(string) default = {} }
