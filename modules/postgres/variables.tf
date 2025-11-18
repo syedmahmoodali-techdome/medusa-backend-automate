64132fc862c14057b42aea141b84384dff08e564
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:25:09 +0530

    Update variables.tf

--- modules/postgres/variables.tf
+++ modules/postgres/variables.tf
@@ -1,9 +1,7 @@
-variable "clinic_name"   { type = string }
-variable "location"      { type = string }
-variable "resource_group"{ type = string }
-
-variable "db_name"       { type = string }
-variable "db_username"   { type = string }
-variable "db_password"   { type = string }
-
-variable "tags"          { type = map(string) }
+variable "clinic_name" { type = string }
+variable "location" { type = string }
+variable "resource_group" { type = string }
+variable "db_name" { type = string }
+variable "db_username" { type = string }
+variable "db_password" { type = string }
+variable "tags" { type = map(string) default = {} }
