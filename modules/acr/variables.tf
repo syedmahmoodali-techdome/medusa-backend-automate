0fb54517ffc51b3d1d795cc7a7af544f2c7396d1
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:24:04 +0530

    Update variables.tf

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -1,4 +1,4 @@
 variable "acr_name" { type = string }
 variable "resource_group" { type = string }
 variable "location" { type = string }
-variable "tags" { type = map(string) }
+variable "tags" { type = map(string) default = {} }
