f3b7aa1eb7ee7501c07b3ebf063adffe89b99c39
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:23:31 +0530

    Update main.tf

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -1,21 +1,14 @@
 resource "azurerm_container_registry" "acr" {
-  name                     = var.acr_name
-  resource_group_name      = var.resource_group
-  location                 = var.location
-  sku                      = "Basic"
-  admin_enabled            = true
-#   georeplication_locations = []
-  tags                     = var.tags
+  name                = var.acr_name
+  location            = var.location
+  resource_group_name = var.resource_group
+  sku                 = "Basic"
+  admin_enabled       = true
+  tags                = var.tags
 }
 
-output "login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-}
+output "name" { value = azurerm_container_registry.acr.name }
+output "login_server" { value = azurerm_container_registry.acr.login_server }
+output "admin_username" { value = azurerm_container_registry.acr.admin_username }
+# admin passwords are only available via azure cli in pipeline; we still expose empty placeholder
+output "admin_password" { value = "" }
