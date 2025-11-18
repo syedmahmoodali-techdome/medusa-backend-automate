34b6ba8b16e43b7555e2141f70dc86293ffb3547
syedmahmoodali-techdome <syed.mahmood.ali@techdome.net.in>
2025-11-19 01:35:01 +0530

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
-  tags     = var.tags
 }
 
 output "name" { value = azurerm_resource_group.rg.name }
